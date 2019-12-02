faa65ef3ecd61fa08e51ad2c89f721849268be2b
SuperSLD <solyanoy.leonid@gmail.com>
2019-12-02 17:55:40 +0300

    Gradle правка

--- app/src/main/java/com/raspisanie/mai/Adapters/TimeTableAdapter.java
+++ app/src/main/java/com/raspisanie/mai/Adapters/TimeTableAdapter.java
@@ -1,6 +1,8 @@
 package com.raspisanie.mai.Adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,7 @@ import com.raspisanie.mai.R;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class TimeTableAdapter extends BaseAdapter {
+public class TimeTableAdapter extends RecyclerView.Adapter {
     private Context ctx;
     private LayoutInflater lInflater;
     private ArrayList<Day> objects;
@@ -28,14 +30,15 @@ public class TimeTableAdapter extends BaseAdapter {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
+    @NonNull
     @Override
-    public int getCount() {
-        return objects.size();
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
     }
 
     @Override
-    public Object getItem(int i) {
-        return objects.get(i);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
     }
 
     @Override
@@ -43,8 +46,13 @@ public class TimeTableAdapter extends BaseAdapter {
         return 0;
     }
 
-    // пункт списка
     @Override
+    public int getItemCount() {
+        return objects.size();
+    }
+
+    // пункт списка
+    //@Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // используем созданные, но не используемые view
         View view =  lInflater.inflate(R.layout.item_table_main, parent, false);
