33722a3121d815d7d74c9ca8b1c670550af1c085
SuperSLD <solyanoy.leonid@gmail.com>
2021-01-05 21:29:19 +0300

    пофиксил импорт

--- MAI2/app/src/main/java/com/raspisanie/mai/ui/main/info/campus_map/CampusMapFragment.kt
+++ MAI2/app/src/main/java/com/raspisanie/mai/ui/main/info/campus_map/CampusMapFragment.kt
@@ -3,16 +3,12 @@ package com.raspisanie.mai.ui.main.info.campus_map
 import android.os.Bundle
 import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.presenter.InjectPresenter
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapsInitializer
 import com.raspisanie.mai.R
 import com.raspisanie.mai.common.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_campus_map.*
 import timber.log.Timber
 
 class CampusMapFragment : BaseFragment(R.layout.fragment_campus_map), MvpView {
-
-    private lateinit var map: GoogleMap
 
     @InjectPresenter
     lateinit var presenter: CampusMapPresenter
