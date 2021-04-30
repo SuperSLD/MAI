1bbe6b131b5e8bde4bcd3b9e85f7aaba899a4411
SuperSLD <solyanoy.leonid@gmail.com>
2021-05-01 02:36:37 +0300

    кастомный прогрес бар готов

--- MAI2/mai2/src/main/java/com/raspisanie/mai/ui/view/ProgressColoredBar.kt
+++ MAI2/mai2/src/main/java/com/raspisanie/mai/ui/view/ProgressColoredBar.kt
@@ -8,8 +8,13 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.animation.doOnEnd
 import androidx.core.animation.doOnRepeat
+import com.smarteist.autoimageslider.IndicatorView.animation.type.ColorAnimation
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
 
 /**
  * Прогрес бар с изменяющимся цветом.
@@ -28,55 +33,83 @@ class ProgressColoredBar: View {
         defUtils
     )
 
-    private val mAnimation: ValueAnimator? = null
     private var mColors = mutableListOf<Int>()
     private var mFirstAngle = 0f
-    private var mSecondAngle = 0f
+    private var mSecondAngle = 30f
+    private var mCurrentColorIndex = 0
     private var mCurrentColor = 0
-    private val mAnimationSet = AnimatorSet()
     private var mFirstAnimator : ValueAnimator? = null
     private var mSecondAnimator : ValueAnimator? = null
-    private var mDelta = 30f
+    private var mColorAnimator: ValueAnimator? = null
+    private var mLastFirstAngle = mFirstAngle
+    private var mLastSecondAngle = mSecondAngle
 
-    private val mDuration = 10000L
-    private val mRotateAngle = 120f
+    private val mWithPart = 0.13f
+    private val mDuration = 1000L
+    private val mDurationColor = 1000L
+    private val mRotateAngle = 220f
 
     init {
         mColors = mutableListOf(
-                Color.parseColor("#0596FF"),
-                Color.parseColor("#A2D8FF"),
-                Color.parseColor("#C7ACFF"),
-                Color.parseColor("#2579C6")
+                Color.parseColor("#EA4335"),
+                Color.parseColor("#FBBC05"),
+                Color.parseColor("#34A853"),
+                Color.parseColor("#4285F4")
         )
 
-        mFirstAnimator = ValueAnimator.ofFloat(mFirstAngle, mFirstAngle + mRotateAngle)
-        mFirstAnimator?.duration = mDuration
-        mFirstAnimator?.addUpdateListener { a ->
-            mFirstAngle = a.animatedValue as Float
-            mSecondAngle = a.animatedValue as Float + mDelta
-            correctAngle()
-            invalidate()
-        }
-        mFirstAnimator?.doOnEnd { mDelta = mSecondAngle - mFirstAngle }
+        mCurrentColor = mColors[0]
 
-
-        mSecondAnimator = ValueAnimator.ofFloat(mSecondAngle, mSecondAngle + mRotateAngle)
-        mSecondAnimator?.duration = mDuration
-        mSecondAnimator?.addUpdateListener { a ->
-            mFirstAngle = a.animatedValue as Float
-            mSecondAngle = a.animatedValue as Float + mDelta
-            correctAngle()
-            invalidate()
-        }
-        mSecondAnimator?.doOnEnd { mDelta = mSecondAngle - mFirstAngle }
-        mSecondAnimator?.doOnEnd {
-            mCurrentColor++
-            if (mCurrentColor == mColors.size) mCurrentColor = 0
+        mFirstAnimator = ValueAnimator.ofFloat(0f, mRotateAngle)
+        with(mFirstAnimator!!) {
+            duration = mDuration
+            repeatCount = 0
+            interpolator = LinearInterpolator()
+            addUpdateListener { a ->
+                mFirstAngle = mLastFirstAngle + a.animatedValue as Float
+                mSecondAngle = mLastSecondAngle + a.animatedValue as Float * 2
+                correctAngle()
+                invalidate()
+            }
+            doOnEnd {
+                mLastFirstAngle = mFirstAngle
+                mLastSecondAngle = mSecondAngle
+                mSecondAnimator?.start()
+            }
         }
 
-        mAnimationSet.play(mSecondAnimator).after(mFirstAnimator)
-        mAnimationSet.doOnEnd { mAnimationSet.start() }
-        mAnimationSet.start()
+        mSecondAnimator = ValueAnimator.ofFloat(0f, mRotateAngle)
+        with(mSecondAnimator!!) {
+            duration = mDuration
+            repeatCount = 0
+            interpolator = LinearInterpolator()
+            addUpdateListener { a ->
+                mFirstAngle = mLastFirstAngle + a.animatedValue as Float * 2
+                mSecondAngle = mLastSecondAngle + a.animatedValue as Float
+                correctAngle()
+                invalidate()
+            }
+            doOnEnd {
+                mCurrentColorIndex++
+                if (mCurrentColorIndex == mColors.size) mCurrentColorIndex = 0
+
+                mColorAnimator = ValueAnimator.ofArgb(
+                        mColors[if (mCurrentColorIndex - 1 < 0) mColors.size - 1 else mCurrentColorIndex - 1],
+                        mColors[mCurrentColorIndex]
+                )
+                with(mColorAnimator!!) {
+                    duration = mDurationColor
+                    repeatCount = 0
+                    addUpdateListener { a -> mCurrentColor = a.animatedValue as Int }
+                    start()
+                }
+
+                mLastFirstAngle = mFirstAngle
+                mLastSecondAngle = mSecondAngle
+                mFirstAnimator?.start()
+            }
+        }
+
+        mFirstAnimator?.start()
     }
 
     /**
@@ -105,18 +138,35 @@ class ProgressColoredBar: View {
         val w = measuredWidth
         mPaint.flags = Paint.ANTI_ALIAS_FLAG
 
-        mPaint.color = mColors[mCurrentColor]
-        canvas!!.drawArc(w / 2 - h / 2.toFloat(), 0f, w / 2 + h / 2.toFloat(), h
-                .toFloat(), mFirstAngle, mSecondAngle, true, mPaint)
+        mPaint.color = mCurrentColor
+        mPaint.style = Paint.Style.STROKE
+        mPaint.strokeWidth = h * mWithPart
+        canvas!!.drawArc(
+                w / 2 - h / 2.toFloat() + h*mWithPart/2,
+                0f  + h*mWithPart/2,
+                w / 2 + h / 2.toFloat() - h*mWithPart/2,
+                h.toFloat() - h*mWithPart/2,
+                mFirstAngle, secondAngleToDelta() - mFirstAngle,
+                false,
+                mPaint
+        )
 
-           /* val angle: Float = data.get(i) * 360 / dataSum
-            paint.setColor(colors.get(i))
-            canvas!!.drawCircle((Math.cos(Math.toRadians(lastAngle + angle.toDouble())) * (h / 2 - h / 20)).toFloat() + w / 2,
-                    (Math.sin(Math.toRadians(lastAngle + angle.toDouble())) * (h / 2 - h / 20)).toFloat() + h / 2,
-                    h / 20.toFloat(), paint)
-            lastAngle += angle*/
-
-        mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-        canvas.drawCircle(w / 2.toFloat(), h / 2.toFloat(), h / 2 - h / 10.toFloat(), mPaint)
+        var lastAngle = 0f
+        mPaint.style = Paint.Style.FILL
+        for (angle in mutableListOf(mFirstAngle, secondAngleToDelta() - mFirstAngle)) {
+            canvas.drawCircle((cos(Math.toRadians(lastAngle + angle.toDouble())) * (h / 2 - h*mWithPart/2)).toFloat() + w / 2,
+                    (sin(Math.toRadians(lastAngle + angle.toDouble())) * (h / 2 - h*mWithPart/2)).toFloat() + h / 2,
+                    h*mWithPart/2, mPaint)
+            lastAngle += angle
+        }
     }
+
+    /**
+     * Приводим угол к виду из которого с ним
+     * удобнее работать для прибавления углов.
+     *
+     * @return mSecondAngle в интервале от 0 дл 360 если
+     * он больше первого угла и +360 если он меньше первого угла.
+     */
+    private fun secondAngleToDelta() = if (mSecondAngle < mFirstAngle) mSecondAngle + 360 else mSecondAngle
 }
